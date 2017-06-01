48b529bdb3ae88f89ba2f1210a2f2775eb01a0dc
Markus Faerevaag <m@frv.ag>
2017-06-02 00:15:21 +0900

    Clean up header imports and exposed functions

--- src/LoopUnrollPass.h
+++ src/LoopUnrollPass.h
@@ -4,29 +4,13 @@
 #include <llvm/Pass.h>
 #include <llvm/Analysis/LoopPass.h>
 
-#include "llvm/Transforms/Scalar.h"
-/* #include "llvm/Constants.h" */
-/* #include "llvm/Function.h" */
-/* #include "llvm/Instructions.h" */
-#include "llvm/Analysis/ConstantFolding.h"
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Analysis/LoopPass.h"
 #include "llvm/Analysis/ScalarEvolution.h"
 #include "llvm/Transforms/Utils/Cloning.h"
 #include "llvm/Transforms/Utils/Local.h"
 #include "llvm/Transforms/Utils/LoopUtils.h"
-/* #include "llvm/Support/CFG.h" */
-#include "llvm/Support/Compiler.h"
 #include "llvm/Support/CommandLine.h"
-#include "llvm/Support/Debug.h"
-#include "llvm/Support/MathExtras.h"
-#include "llvm/ADT/Statistic.h"
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/ADT/SmallPtrSet.h"
-/* #include "llvm/IntrinsicInst.h" */
-#include <algorithm>
-#include <climits>
-#include <cstdio>
 
 using namespace llvm;
 
@@ -42,16 +26,16 @@ class LoopUnroll : public LoopPass
     static const unsigned NoThreshold = UINT_MAX;
 
     bool runOnLoop(Loop *L, LPPassManager &LPM);
-    bool unrollLoop(Loop *L, unsigned Count, unsigned Threshold, LoopInfo *LI, DominatorTree &DT, ScalarEvolution *SE);
-    BasicBlock *FoldBlockIntoPredecessor(BasicBlock *BB, LoopInfo *LI);
 
-    void getAnalysisUsage(AnalysisUsage &AU) const override {
-        /* AU.addRequired<AssumptionCacheTracker>(); */
-        /* AU.addRequired<TargetTransformInfoWrapperPass>(); */
-        // FIXME: Loop passes are required to preserve domtree, and for now we just
-        // recreate dom info if anything gets unrolled.
+    void getAnalysisUsage(AnalysisUsage &AU) const override
+    {
         getLoopAnalysisUsage(AU);
     }
+
+ private:
+    bool unrollLoop(Loop *L, unsigned Count, unsigned Threshold,
+                    LoopInfo *LI, DominatorTree &DT, ScalarEvolution *SE);
+    BasicBlock *FoldBlockIntoPredecessor(BasicBlock *BB, LoopInfo *LI);
 };
 
 #endif /* LOOP_UNROLL_PASS_H */
