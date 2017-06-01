234e7f7b26cb65a1af69b85014a2699cdac7abe5
Markus Faerevaag <m@frv.ag>
2017-06-01 20:19:59 +0900

    Wip use of unrollLoop util func

--- src/LoopUnrollPass.cpp
+++ src/LoopUnrollPass.cpp
@@ -3,6 +3,7 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Transforms/Utils/UnrollLoop.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
@@ -21,8 +22,33 @@ bool LoopUnroll::runOnLoop(Loop *L, LPPassManager &LPM)
         errs() << "Skipping...\n";
     }
 
-    errs() << "Discarding...\n";
-    return false;
+    LoopInfo *LI = NULL; //&getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+
+    unsigned count = 0;
+    unsigned tripCount = 0;
+    bool force = false;
+    bool allowRuntime = false;
+    bool allowExpensiveTripCount = false;
+    bool preserveCondBr = false;
+    bool preserveOnlyFirst = false;
+    unsigned tripMultiple = 0;
+    unsigned peelCount = 0;
+    bool preserveLCSSA = false;
+
+
+    // unroll
+    if (!UnrollLoop(L, count, tripCount, force,
+                    allowRuntime, allowExpensiveTripCount,
+                    preserveCondBr, preserveOnlyFirst,
+                    tripMultiple, peelCount,
+                    LI, NULL, NULL, NULL, NULL,
+                    preserveLCSSA)) {
+        errs() << "Failed to unroll\n";
+        return false;
+    }
+
+    errs() << "Success!\n";
+    return true;
 }
 
 char LoopUnroll::ID = 0;
